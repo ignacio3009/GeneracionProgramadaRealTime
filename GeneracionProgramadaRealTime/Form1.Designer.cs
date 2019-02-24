5f692f53c176fe716aa465dbf0f9c5fee9c995d2
Ignacio Ñancupil <ignacio.nancupil@ug.uchile.cl>
2019-02-24 11:45:08 -0300

    Inicio

--- GeneracionProgramadaRealTime/Form1.Designer.cs
+++ GeneracionProgramadaRealTime/Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambiarCarpetaDePoliticasYProgramasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seleccionarCentralesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainPanel = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CheckedBoxCentrales = new System.Windows.Forms.CheckedListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(506, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cambiarCarpetaDePoliticasYProgramasToolStripMenuItem,
+            this.seleccionarCentralesToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // cambiarCarpetaDePoliticasYProgramasToolStripMenuItem
+            // 
+            this.cambiarCarpetaDePoliticasYProgramasToolStripMenuItem.Name = "cambiarCarpetaDePoliticasYProgramasToolStripMenuItem";
+            this.cambiarCarpetaDePoliticasYProgramasToolStripMenuItem.Size = new System.Drawing.Size(295, 22);
+            this.cambiarCarpetaDePoliticasYProgramasToolStripMenuItem.Text = "Cambiar Carpeta de Politicas y Programas";
+            // 
+            // seleccionarCentralesToolStripMenuItem
+            // 
+            this.seleccionarCentralesToolStripMenuItem.Name = "seleccionarCentralesToolStripMenuItem";
+            this.seleccionarCentralesToolStripMenuItem.Size = new System.Drawing.Size(295, 22);
+            this.seleccionarCentralesToolStripMenuItem.Text = "Seleccionar Centrales";
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.MainPanel.GridLines = true;
+            this.MainPanel.Location = new System.Drawing.Point(0, 27);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(320, 388);
+            this.MainPanel.TabIndex = 1;
+            this.MainPanel.UseCompatibleStateImageBehavior = false;
+            this.MainPanel.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Central";
+            this.columnHeader1.Width = 147;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Gen Prog [MW]";
+            this.columnHeader2.Width = 169;
+            // 
+            // CheckedBoxCentrales
+            // 
+            this.CheckedBoxCentrales.FormattingEnabled = true;
+            this.CheckedBoxCentrales.Items.AddRange(new object[] {
+            "Rapel",
+            "Ralco",
+            "ElToro",
+            "Cipreses",
+            "Pangue",
+            "Canutillar",
+            "Pehuenche",
+            "Abanico",
+            "Colbún"});
+            this.CheckedBoxCentrales.Location = new System.Drawing.Point(326, 27);
+            this.CheckedBoxCentrales.Name = "CheckedBoxCentrales";
+            this.CheckedBoxCentrales.Size = new System.Drawing.Size(162, 379);
+            this.CheckedBoxCentrales.TabIndex = 2;
+            this.CheckedBoxCentrales.SelectedIndexChanged += new System.EventHandler(this.CheckedBoxCentrales_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(506, 412);
+            this.Controls.Add(this.CheckedBoxCentrales);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Generacion Programada CEN";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambiarCarpetaDePoliticasYProgramasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seleccionarCentralesToolStripMenuItem;
+        private System.Windows.Forms.ListView MainPanel;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.CheckedListBox CheckedBoxCentrales;
     }
 }
 
--- GeneracionProgramadaRealTime/Form1.cs
+++ GeneracionProgramadaRealTime/Form1.cs
@@ -1,20 +1,137 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace GeneracionProgramadaRealTime
 {
     public partial class Form1 : Form
     {
+        string[] Data;
+        string[] Names = new string[10];
+        double[,] Gen = new double[10,24];
         public Form1()
         {
             InitializeComponent();
+            populateNames();
+            resetGen();
+            getData();
+
+
         }
+        
+        private void resetGen()
+        {
+            //Console.WriteLine(Gen.Length);
+            for(int i=0;i<10;i++)
+            {
+                for(int j = 0; j < 24; j++)
+                {
+                    Gen[i, j] = 0;
+                }
+            }
+        }
+
+        private void CheckedBoxCentrales_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MainPanel.Items.Clear();
+            int hora = DateTime.Now.Hour;
+            for (int i = 0; i < CheckedBoxCentrales.Items.Count; i++)
+            {
+                if (CheckedBoxCentrales.GetItemChecked(i))
+                {
+                    if (Names[i] == null) break;
+                    ListViewItem item = new ListViewItem();
+                    item.Text = (string)CheckedBoxCentrales.Items[i];
+                    item.SubItems.Add(Gen[i,hora].ToString());
+                    MainPanel.Items.Add(item);
+                }
+
+
+            }
+        }
+
+        private void populateNames()
+        {
+            for (int i = 0; i < CheckedBoxCentrales.Items.Count; i++)
+            {
+                Names[i] = CheckedBoxCentrales.Items[i].ToString().ToLower();
+            }
+        }
+
+        public void getData()
+        {
+            string name = "PRG" + DateTime.Now.ToString("yy") + DateTime.Now.ToString("MM") + DateTime.Now.ToString("dd") + ".xlsx";
+            string path = @"C:\Users\ignac\source\repos\GeneracionProgramadaRealTime\GeneracionProgramadaRealTime\bin\Debug\";
+            string filename = path + name;
+            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(filename)))
+            {
+                resetGen();
+                var myWorksheet = xlPackage.Workbook.Worksheets.First(); //select sheet here
+                var totalRows = myWorksheet.Dimension.End.Row;
+                var totalColumns = myWorksheet.Dimension.End.Column;
+
+                var sb = new StringBuilder(); //this is your your data
+                for (int rowNum = 1; rowNum <= totalRows; rowNum++) //selet starting row here
+                {
+                    var row = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                    sb.AppendLine(string.Join(",", row));
+                }
+                Data = sb.ToString().ToLower().Split();
+                //Console.WriteLine(Data[100]);
+
+                for (int i = 0; i < Names.Length; i++)
+                {
+                    if (Names[i] == null) break;
+
+                    string central = Names[i];
+
+                    for (int j = 0; j < Data.Length; j++)
+                    {
+
+                        string[] linea = Data[j].Split(',');
+
+
+                        if (Data[j].Contains("trayectoria"))
+                        {
+                            break;
+                        }
+                        if (linea[0].Contains(central))
+                        {
+                            for (int h = 0; h < 24; h++)
+                            {
+                                //suma = suma + Double.Parse(linea[2]);
+                                Gen[i, h] = Gen[i, h] + Double.Parse(linea[h + 2]);
+                            }
+
+                        }
+                    }
+                }
+                fillDataMainPanel();
+            }
+        }
+
+        public void fillDataMainPanel()
+        {
+            int hora = DateTime.Now.Hour;
+            for (int i = 0; i < Names.Length; i++)
+            {   
+                if (Names[i] == null) break;
+                ListViewItem item = new ListViewItem();
+                item.Text = (string)CheckedBoxCentrales.Items[i];
+                item.SubItems.Add(Gen[i, hora].ToString());
+                MainPanel.Items.Add(item);
+            }
+        }
+
     }
 }
